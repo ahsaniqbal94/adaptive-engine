36c2a642646ea4e5a96e32c6f62928aef8221894
Ahsan Iqbal <ahsan.iqbal@securiti.ai>
2020-06-13 04:32:16 +0500

    Adding domain specific host to allowed hosts

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -6,7 +6,7 @@ INSTALLED_APPS += [
     'django_extensions',
 ]
 
-ALLOWED_HOSTS += ['localhost', 'engine', 'engine.lms','bridge']
+ALLOWED_HOSTS += ['localhost', 'engine', 'engine.lms', 'bridge', '*.schooltutor.net', 'schooltutor.net']
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
